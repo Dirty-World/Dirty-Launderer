a05e85fdaf8e3edbcba594d269c4c34f063cc527
The Dirty Launderer <the-dirty-launderer@gmail.com>
2025-06-06 22:00:27 -0500

    fix: add __init__.py to utils package for proper Python package structure

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,5 +1,6 @@
 """Utils package for The Dirty Launderer bot.""" 
 
 from .rate_limiter import check_rate_limit, cleanup_session
+from .input_sanitizer import sanitize_input, get_safe_domain
 
-__all__ = ['check_rate_limit', 'cleanup_session'] 
+__all__ = ['check_rate_limit', 'cleanup_session', 'sanitize_input', 'get_safe_domain'] 

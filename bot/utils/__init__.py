50e303eaa10e172fcedf2265c1d25f7d9d4492df
The Dirty Launderer <the-dirty-launderer@gmail.com>
2025-06-06 22:30:52 -0500

    refactor: remove direct Firestore dependencies and clean up imports

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,4 +1,7 @@
-"""Utils package for The Dirty Launderer bot.""" 
+"""
+Utils package for The Dirty Launderer bot.
+Contains helper functions and utilities for the bot's operation.
+""" 
 
 from .rate_limiter import check_rate_limit, cleanup_session
 from .input_sanitizer import sanitize_input, get_safe_domain
